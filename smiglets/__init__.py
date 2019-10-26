e8c00631783acc736dead26e45305d5115fef983
wizardjet <cx.jet97@yahoo.co.uk>
2019-10-26 02:18:00 +0100

    created registration page

--- smiglets/__init__.py
+++ smiglets/__init__.py
@@ -6,4 +6,4 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-# from smiglets import routes
+from smiglets import routes
--- smiglets/forms.py
+++ smiglets/forms.py
@@ -1,16 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+    first_name = StringField('First Name',
+                           validators=[DataRequired(), Length(min=1, max=50)])
+    last_name = StringField('Last Name',
+                           validators=[DataRequired(), Length(min=0, max=50)])
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+                        validators=[DataRequired(), Length(min=0, max=10)])
+    year_of_study = SelectField('Year of Study', choices=[("Foundation","Foundation"), ("1st Year", "1st Year"), ("2nd Year", "2nd Year"), ("3rd Year", "3rd Year"),  ("4th Year", "4th Year"), ("Masters", "Masters"), ("PhD", "PhD"), ("Alumni", "Alumni")])
+    course = StringField('Course',
+                           validators=[DataRequired(), Length(min=0, max=50)])
+    malaysian = BooleanField('Malaysian?')
+    membership = BooleanField('Would you like a membership?')
+
+    # password = PasswordField('Password', validators=[DataRequired()])
+    # confirm_password = PasswordField('Confirm Password',
+    #                                  validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Join')
 
 
 class LoginForm(FlaskForm):
